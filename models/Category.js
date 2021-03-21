1fd0bcd5c1a142a6b5e239d7823e2ca7ec71c6f6
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-03-21 14:30:43 -0700

    defined category columns

--- models/Category.js
+++ models/Category.js
@@ -7,22 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
-    
-
-
-
-
-// id
-// Integer
-// Doesn't allow null values.
-// Set as primary key.
-// Uses auto increment.
-
-// category_name
-// String.
-// Doesn't allow null values.
-
-
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false ,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
