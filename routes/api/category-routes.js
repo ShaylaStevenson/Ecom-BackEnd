a4b3fe3d7be80d4e648b22d92b224cebe07b1289
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-03-24 14:19:12 -0700

    added post routes for category and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,8 +47,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+
+  // activity 28
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
