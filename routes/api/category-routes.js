09fe4b9ba550b1dbf8190f89876c24f4e8bc50bc
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-03-24 12:37:38 -0700

    get all categories route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,14 +6,20 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  // https://sequelize.org/master/manual/model-querying-basics.html
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }],
+      include: [
+        {
+          model: Product,
+        },
+      ],
     });
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(categoryData);
-  }
+    res.status(500).json(err);
+  }  
 });
 
 router.get('/:id', (req, res) => {
